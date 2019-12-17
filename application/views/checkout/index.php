a7a8d6b26b2c14cd88055ded0461533bbb5ec17e
mikerosevelt <zoe_abbas@aim.com>
2019-12-17 08:21:29 +0700

    Success method

--- application/views/checkout/index.php
+++ application/views/checkout/index.php
@@ -1,153 +1,153 @@
     <div class="hero-wrap hero-bread" style="background-image: url('http://localhost/flower/assets/images/bg_1.jpg');">
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Checkout</span></p>
-            <h1 class="mb-0 bread">Checkout</h1>
-          </div>
-        </div>
-      </div>
+    	<div class="container">
+    		<div class="row no-gutters slider-text align-items-center justify-content-center">
+    			<div class="col-md-9 ftco-animate text-center">
+    				<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Checkout</span></p>
+    				<h1 class="mb-0 bread">Checkout</h1>
+    			</div>
+    		</div>
+    	</div>
     </div>
 
     <section class="ftco-section">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xl-7 ftco-animate">
-						<form action="#" class="billing-form">
-							<h3 class="mb-4 billing-heading">Shipping Details</h3>
-	          	<div class="row align-items-end">
-	          		<div class="col-md-12">
-	                <div class="form-group">
-	                	<label for="firstname">Full Name</label>
-	                  <input type="text" class="form-control" placeholder="" value="<?= $user['name']; ?>">
-	                </div>
-	              </div>
-                <div class="w-100"></div>
-		            <div class="col-md-12">
-		            	<div class="form-group">
-		            		<label for="country">State / Country</label>
-		            		<div class="select-wrap">
-		                  <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-		                  <select name="" id="" class="form-control">
-		                  	<option value="">Indonesia</option>
-		                    <option value="">Italy</option>
-		                    <option value="">Philippines</option>
-		                    <option value="">South Korea</option>
-		                    <option value="">Hongkong</option>
-		                    <option value="">Japan</option>
-		                  </select>
-		                </div>
-		            	</div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                	<label for="streetaddress">Street Address</label>
-	                  <input type="text" class="form-control" placeholder="House number and street name">
-	                </div>
-		            </div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Appartment, suite, unit etc: (optional)">
-	                </div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                	<label for="towncity">Town / City</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-		            </div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-		            		<label for="postcodezip">Postcode / ZIP *</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="phone">Phone</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-	              </div>
-	              <div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="emailaddress">Email Address</label>
-	                  <input type="text" class="form-control" placeholder="" value="<?= $user['email']; ?>">
-	                </div>
-                </div>
-                <div class="w-100"></div>
-                <div class="col-md-6">
-                	<div class="form-group mt-4">
-										<div class="radio">
-										  <label><input type="checkbox" name="shipping" checked> Same as billing address ?</label><br>
-										  <label class="mr-3"><input type="radio" name="optradio"> Express Shipping (1 - 2 Day) </label><br>
-										  <label><input type="radio" name="optradio" checked> Regular Shipping (3 - 5 Day) </label>
-										</div>
-									</div>
-                </div>
-	            </div>
-	          </form><!-- END -->
-					</div>
-					<div class="col-xl-5">
-	          <div class="row mt-5 pt-3">
-	          	<div class="col-md-12 d-flex mb-5">
-	          		<div class="cart-detail cart-total p-3 p-md-4">
-	          			<h3 class="billing-heading mb-4">Cart Total</h3>
-	          			<p class="d-flex">
-		    						<span>Subtotal</span>
-		    						<span>Rp.<?= number_format($this->cart->total(), 0,",","."); ?></span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Delivery</span>
-		    						<span>Free</span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Discount</span>
-		    						<span>Rp.0</span>
-		    					</p>
-		    					<hr>
-		    					<p class="d-flex total-price">
-		    						<span>Total</span>
-		    						<span>Rp.<?= number_format($this->cart->total(), 0,",","."); ?></span>
-		    					</p>
-								</div>
-	          	</div>
-	          	<div class="col-md-12">
-	          		<div class="cart-detail p-3 p-md-4">
-	          			<h3 class="billing-heading mb-4">Payment Method</h3>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="radio">
-											   <label>
-											   	<input type="radio" name="optradio" class="mr-2"> Direct Bank Tranfer
-											   </label>
-											</div>
-											<div class="radio">
-											   <label>
-											   	<input type="radio" name="optradio" class="mr-2"> Credit Cart
-											   </label>
-											</div>
-											<div class="radio">
-											   <label>
-											   	<input type="radio" name="optradio" class="mr-2"> Paypal
-											   </label>
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="col-md-12">
-											<div class="checkbox">
-											   <label><input type="checkbox" value="" class="mr-2"> I have read and accept the terms and conditions</label>
-											</div>
-										</div>
-									</div>
-									<p><a href="#"class="btn btn-primary py-3 px-4">Place an order</a></p>
-								</div>
-	          	</div>
-	          </div>
-          </div> <!-- .col-md-8 -->
-        </div>
-      </div>
+    	<div class="container">
+    		<div class="row justify-content-center">
+    			<div class="col-xl-7 ftco-animate">
+    				<form action="#" class="billing-form">
+    					<h3 class="mb-4 billing-heading">Shipping Details</h3>
+    					<div class="row align-items-end">
+    						<div class="col-md-12">
+    							<div class="form-group">
+    								<label for="firstname">Full Name</label>
+    								<input type="text" class="form-control" placeholder="" value="<?= $user['name']; ?>">
+    							</div>
+    						</div>
+    						<div class="w-100"></div>
+    						<div class="col-md-12">
+    							<div class="form-group">
+    								<label for="country">State / Country</label>
+    								<div class="select-wrap">
+    									<div class="icon"><span class="ion-ios-arrow-down"></span></div>
+    									<select name="" id="" class="form-control">
+    										<option value="">Indonesia</option>
+    										<option value="">Italy</option>
+    										<option value="">Philippines</option>
+    										<option value="">South Korea</option>
+    										<option value="">Hongkong</option>
+    										<option value="">Japan</option>
+    									</select>
+    								</div>
+    							</div>
+    						</div>
+    						<div class="w-100"></div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<label for="streetaddress">Street Address</label>
+    								<input type="text" class="form-control" placeholder="House number and street name">
+    							</div>
+    						</div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<input type="text" class="form-control" placeholder="Appartment, suite, unit etc: (optional)">
+    							</div>
+    						</div>
+    						<div class="w-100"></div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<label for="towncity">Town / City</label>
+    								<input type="text" class="form-control" placeholder="">
+    							</div>
+    						</div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<label for="postcodezip">Postcode / ZIP *</label>
+    								<input type="text" class="form-control" placeholder="">
+    							</div>
+    						</div>
+    						<div class="w-100"></div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<label for="phone">Phone</label>
+    								<input type="text" class="form-control" placeholder="">
+    							</div>
+    						</div>
+    						<div class="col-md-6">
+    							<div class="form-group">
+    								<label for="emailaddress">Email Address</label>
+    								<input type="text" class="form-control" placeholder="" value="<?= $user['email']; ?>">
+    							</div>
+    						</div>
+    						<div class="w-100"></div>
+    						<div class="col-md-6">
+    							<div class="form-group mt-4">
+    								<div class="radio">
+    									<label><input type="checkbox" name="shipping" checked> Same as billing address ?</label><br>
+    									<label class="mr-3"><input type="radio" name="optradio"> Express Shipping (1 - 2 Day) </label><br>
+    									<label><input type="radio" name="optradio" checked> Regular Shipping (3 - 5 Day) </label>
+    								</div>
+    							</div>
+    						</div>
+    					</div>
+    				</form><!-- END -->
+    			</div>
+    			<div class="col-xl-5">
+    				<div class="row mt-5 pt-3">
+    					<div class="col-md-12 d-flex mb-5">
+    						<div class="cart-detail cart-total p-3 p-md-4">
+    							<h3 class="billing-heading mb-4">Cart Total</h3>
+    							<p class="d-flex">
+    								<span>Subtotal</span>
+    								<span>Rp.<?= number_format($this->cart->total(), 0, ",", "."); ?></span>
+    							</p>
+    							<p class="d-flex">
+    								<span>Delivery</span>
+    								<span>Free</span>
+    							</p>
+    							<p class="d-flex">
+    								<span>Discount</span>
+    								<span>Rp.0</span>
+    							</p>
+    							<hr>
+    							<p class="d-flex total-price">
+    								<span>Total</span>
+    								<span>Rp.<?= number_format($this->cart->total(), 0, ",", "."); ?></span>
+    							</p>
+    						</div>
+    					</div>
+    					<div class="col-md-12">
+    						<div class="cart-detail p-3 p-md-4">
+    							<h3 class="billing-heading mb-4">Payment Method</h3>
+    							<div class="form-group">
+    								<div class="col-md-12">
+    									<div class="radio">
+    										<label>
+    											<input type="radio" name="optradio" class="mr-2"> Direct Bank Tranfer
+    										</label>
+    									</div>
+    									<div class="radio">
+    										<label>
+    											<input type="radio" name="optradio" class="mr-2"> Credit Cart
+    										</label>
+    									</div>
+    									<div class="radio">
+    										<label>
+    											<input type="radio" name="optradio" class="mr-2"> Paypal
+    										</label>
+    									</div>
+    								</div>
+    							</div>
+    							<div class="form-group">
+    								<div class="col-md-12">
+    									<div class="checkbox">
+    										<label><input type="checkbox" value="" class="mr-2"> I have read and accept the terms and conditions</label>
+    									</div>
+    								</div>
+    							</div>
+    							<p><a href="<? base_url('cart/success'); ?>" class="btn btn-primary py-3 px-4">Place an order</a></p>
+    						</div>
+    					</div>
+    				</div>
+    			</div> <!-- .col-md-8 -->
+    		</div>
+    	</div>
     </section> <!-- .section -->
