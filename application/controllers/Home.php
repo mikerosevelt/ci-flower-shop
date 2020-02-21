59231e19caed904109a8a3adbe18ecaa5a1c76f9
mikerosevelt <zoe_abbas@aim.com>
2020-02-21 13:20:23 +0700

    add subscribe newsletter function

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -30,4 +30,23 @@ class Home extends CI_Controller {
 		$this->load->view('home/contact');
 		$this->load->view('templates/main_footer');
 	}
+
+	public function subscribe()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[newsletter.email]', [
+            'is_unique' => 'Email is already joined!'
+        ]);
+        if ($this->form_validation->run() == FALSE) {
+        	$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is already joined!.</div>');
+        	redirect();
+        } else {
+	        $data = [
+				'email' => $this->input->post('email'),
+				'date_join' => time()
+			];
+			$this->db->insert('newsletter', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Thank you for join our newsletter.</div>');
+			redirect();
+        }
+	}
 }
